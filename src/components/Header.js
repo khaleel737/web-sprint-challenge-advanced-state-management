71b993fbdc613f2a5f089f9703fafb154f57f756
Khaleel Musleh <khaleelmusleh@Khaleels-MacBook-Pro.local>
2022-01-21 15:43:11 -0800

    almost done

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Header = ()=>{
     return(<nav className="navbar navbar-dark bg-primary">
-        <a className="navbar-brand">Smurf Village Database</a>
+        <href className="navbar-brand">Smurf Village Database</href>
     </nav>);
 }
 
